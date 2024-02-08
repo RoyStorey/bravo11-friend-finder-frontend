67b677122ce5a594fcdcd5af2037855ac04800ff
RoyStorey <60338602+RoyStorey@users.noreply.github.com>
2024-02-08 03:28:35 -0500

    color scheme changeup

--- src/components/pitch-container.jsx
+++ src/components/pitch-container.jsx
@@ -6,14 +6,14 @@ export default function Pitch() {
     <div className="pitch-container">
       <div className="pitch-content">
         <h1 className="pitch-header">The Pitch</h1>
-        <p>
+        <p className="pitch-text">
           On Monday, we all sat through 2 hours of pitches, around 100 pitches.
           Afterwards, when we broke out into the CUI and Secret areas, we
           realized that there was a huge issue. There was no centralized web-app
           where we could see the teams that were already formed and working on a
           project.
         </p>
-        <p>
+        <p className="pitch-text">
           We were sitting in one of the cubicles, not wanting to talk to anyone,
           and wishing that there was an easy way to find the team that was
           tackling one of the interesting problems. We decided that somebody
@@ -21,6 +21,7 @@ export default function Pitch() {
           use-case and be able to find them easily.
         </p>
         <p>We made this app.</p>
+        <p>...and so did another team.</p>
       </div>
     </div>
   );
