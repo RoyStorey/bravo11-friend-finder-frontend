b4490fa2f16949cfe6369824fc162c268898521a
RoyStorey <imlejiii@gmail.com>
2024-02-12 22:57:05 -0500

    scss-ified css files

--- src/components/sadUseCasesTable.jsx
+++ src/components/sadUseCasesTable.jsx
@@ -24,6 +24,7 @@ export default function SadUseCasesTable({ data }) {
       columns,
       data,
     });
+
   if (!data || data.length === 0) {
     return <p>No use cases with no teams found.</p>;
   }
