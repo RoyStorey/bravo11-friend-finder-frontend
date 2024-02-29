31a28ccbdda040c3781486e1c6fcb0da277712c2
Roy Storey <yourbuddyroy@gmail.com>
2024-02-29 19:02:33 +0000

    frontend mobile updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,49 +9,80 @@ import {
   faSadCry,
   faUserGroup,
   faList,
+  faHamburger,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 
+function Modal() {
+  return (
+    <div className="modal" id="modal">
+      <div className="modal-header">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="modal-icon"
+          onClick={toggleModal}
+        />
+      </div>
+      <div className="modal-content">
+        <a href="/">Teams</a>
+        <a href="/tasks">Tasks</a>
+        <a href="/add-team/">Add Team</a>
+        <a href="/add-task/">Add Task</a>
+      </div>
+    </div>
+  );
+}
 
+function toggleModal() {
+  let modal = document.getElementById("modal");
+  if (modal) {
+    modal.classList.toggle("active");
+  }
+}
 
 export default function Header() {
   const navigate = useNavigate();
 
   return (
     <div className="header-container">
+      <Modal />
       <title>
         <div className="icon-container">
-          <h1
-            onClick={() => navigate(`/`)}
-          >
-            Bravo 11 Team Builder
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={() => toggleModal()}
+            className="header-icon mobile-only"
+          />
+          <h1 onClick={() => navigate(`/`)} className="desktop-only">
+            Team Builder
           </h1>
           <FontAwesomeIcon icon={faHorseHead} className="header-icon" />
         </div>
       </title>
-      <div className="hyperlinks">
+      <div className="hyperlinks desktop-only">
         <div className="button-group">
           <a onClick={() => navigate(`/`)}>
             <FontAwesomeIcon icon={faUserGroup} className="hyperlink-icon" />
-            Teams
+            <p className="desktop-only">Teams</p>
           </a>
           <a onClick={() => navigate(`/tasks`)}>
             <FontAwesomeIcon icon={faList} className="hyperlink-icon" />
-            Tasks
+            <p className="desktop-only">Tasks</p>
           </a>
         </div>
         <a onClick={() => navigate(`/sad-boy-hours`)}>
           <FontAwesomeIcon icon={faSadCry} className="hyperlink-icon" />
-          Help, I'm sad.
+          <p className="desktop-only">Help, I'm sad.</p>
         </a>
 
         <div className="button-group">
           <a onClick={() => navigate(`/add-team`)}>
             <FontAwesomeIcon icon={faPlus} />
-            Add Team
+            <p className="desktop-only">Add Team</p>
           </a>
           <a onClick={() => navigate(`/add-task`)}>
             <FontAwesomeIcon icon={faPlus} />
-            Add Task
+            <p className="desktop-only">Add Task</p>
           </a>
         </div>
       </div>
